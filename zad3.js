a7bdfcdc79b12e5e52164f905d0d7684fc912eb1
Milosz <milosz.moska@dige.pl>
2018-10-29 13:24:12 +0100

    zad3 method bind

--- zad3.js
+++ zad3.js
@@ -4,25 +4,28 @@ class Zad3 extends Base {
     super();
     this.index = [];
     this.data = [];
+    this.fs = 4;
     this.trace = {
       x: this.index,
       y: this.data,
       type: 'scatter'
     };
     this.figure = { data: [this.trace] };
+    this.second = this.second.bind(this);
+    this.fourth = this.fourth.bind(this);
   }
   first(n) {
     return Math.pow(0.8, n) - 1;
   }
   second(n) {
-    const fs = 4;
+    const { fs } = this;
     return -1.8 * n * Math.cos(16 * Math.PI * (n / fs) + Math.PI);
   }
   third(n) {
     return 0.72 * n;
   }
   fourth(n) {
-    const fs = 4;
+    const { fs } = this;
     return 0.29 * Math.pow(n, 8) * Math.sin(31 * Math.PI * (n / fs)) + 0.55;
   }
   count() {
